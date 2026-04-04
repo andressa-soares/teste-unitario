d34f6c614c7ddca3767b75b51a939bd84f46b6fe
Andressa Soares <andressa012215@gmail.com>
2026-04-04 20:37:54 -0300

    green: implementa depositar com validação - status da conta

--- src/main/java/contabancaria/Conta.java
+++ src/main/java/contabancaria/Conta.java
@@ -65,6 +65,8 @@ public class Conta {
     public void depositar(double valor) {
         if (valor <= 0)
             throw new IllegalArgumentException("O valor deve ser maior que zero.");
+        if (this.ativa == false)
+            throw new IllegalStateException("A conta está inativa.");
 
         this.saldo += valor;
     }
